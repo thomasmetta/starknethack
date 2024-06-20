f0403b3429abf3f4597c3a32b7a08b1077ba7ef0
Thomas Lui <thomaslui@Thomass-MacBook-Pro-2.local>
2024-06-19 22:11:15 -0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { DynamicContextProvider, DynamicWidget, FilterChain } from "@dynamic-labs/sdk-react-core";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { StarknetWalletConnectors } from "@dynamic-labs/starknet";
+
+import SignMessage from './SignMessage.tsx';
+
+import './App.css';
+
+import {
+  StarknetIcon,
+  EthereumIcon,
+} from '@dynamic-labs/iconic';
+
+const App = () => {
+
+  const locale = {
+    en: {
+      dyn_login: {
+        title: {
+          all: "Hello",
+        },
+      },
+    },
+  };
+  
+  const EthWallets = {
+    label: { icon: <EthereumIcon /> },
+    walletsFilter: FilterChain('EVM'),
+    recommendedWallets: [
+      {
+        walletKey: 'metamask',
+      },
+    ],
+  };
+
+  const StarkWallets = {
+    label: { icon: <StarknetIcon /> },
+    walletsFilter: FilterChain('STARK'),
+    recommendedWallets: [
+      {
+        walletKey: 'braavos',
+        label: 'New'
+      },
+    ],
+  };
+
+  const views = [
+    {
+      type: 'wallet-list',
+      tabs: {
+        items: [
+          EthWallets,
+          StarkWallets,
+        ]
+      }
+    }
+  ];
+
+  return (
+    <DynamicContextProvider
+        settings={{
+          environmentId: "",
+          walletConnectors: [EthereumWalletConnectors, StarknetWalletConnectors],
+          overrides: {
+            views: views,
+          },
+      }}
+
+      locale={locale}
+    >
+      <DynamicWidget />
+      <SignMessage />
+    </DynamicContextProvider>
+  )
+}
+
+export default App;
